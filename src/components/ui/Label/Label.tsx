8d8b102c348232348f4cf78d1bb00bc096f73b7c
tomek-i <tomek.iwainski@gmail.com>
2024-08-27 04:12:25 +1000

    chore: Update Label component to include required prop and fix formatting

--- src/components/ui/Label/Label.tsx
+++ src/components/ui/Label/Label.tsx
@@ -1,20 +1,27 @@
 import React from "react"
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { label ,type LabelVariants } from "./Label.variants"
+import { label, type LabelVariants } from "./Label.variants"
 
-export type LabelProps = { disabled?: boolean } 
-  & React.HTMLAttributes<HTMLDivElement> 
-  & LabelVariants
+export type LabelProps = { disabled?: boolean; required?: boolean } & React.LabelHTMLAttributes<HTMLLabelElement> &
+  LabelVariants
 
 export const Label: React.FC<LabelProps> = ({
   className = "",
   size = "default",
   variant = "default",
   disabled = false,
+  required = false,
+  htmlFor = undefined,
+  children,
   ...props
 }) => {
   const style = clsx(label({ variant, disabled, size }))
-  
-  return <div className={twMerge(style, className)} {...props}></div>
+
+  return (
+    <label htmlFor={htmlFor} className={twMerge(style, className)} {...props}>
+      {children}
+      {required && <span className="text-red-500">*</span>}
+    </label>
+  )
 }
