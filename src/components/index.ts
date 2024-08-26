12cc73d19d69e9ce3371c6f563e9b0b1a434f73d
tomek-i <tomek.iwainski@gmail.com>
2024-08-27 03:59:54 +1000

    refactor: renamed .todo to index.ts files to trigger TODO bot

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+// TODO: create Card component
+// TODO: create Modal component using the new html dialog element
+// TODO: create Overlay element with blur/glass effect
+// TODO: create Dropdown / Select with options, auto-complete
---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+// TODO: create label component
+// TODO: create textarea component
+// TODO: create input with label component
+// TODO: create checkbox component
+// TODO: create toggle component
+// TODO: create form component
