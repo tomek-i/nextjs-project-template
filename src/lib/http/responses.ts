9da45a415d83b01a962d2f1930769709075e80f5
tomek-i <tomek.iwainski@gmail.com>
2024-10-18 15:21:23 +1100

    Refactor response functions to use Next.js server response

--- src/lib/http/responses.ts
+++ src/lib/http/responses.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server"
+
 export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD" | "CONNECT" | "TRACE"
 
 type errorResponse<T> = {
@@ -18,37 +20,38 @@ const error = <T>(payload: T) => {
 }
 
 export function OK<T>(payload?: T) {
-  return Response.json(success(payload), { status: 200 })
+  if (!payload) return NextResponse.json(null, { status: 200 })
+  return NextResponse.json(success(payload), { status: 200 })
 }
 
 //TODO: need to add headers for the response to locate the created resourceb y id
 export function Created<T>(payload: T, location?: string) {
   const headers = location ? { location: location } : undefined
-  return Response.json(success(payload), { status: 201, headers })
+  return NextResponse.json(success(payload), { status: 201, headers })
 }
 
 export function Conflict<T>(payload: T) {
-  return Response.json(error(payload), { status: 409 })
+  return NextResponse.json(error(payload), { status: 409 })
 }
 
 export function ServerError<T>(payload: T) {
-  return Response.json(error(payload), { status: 500 })
+  return NextResponse.json(error(payload), { status: 500 })
 }
 
 export function NoContent() {
-  return new Response(null, {
+  return new NextResponse(null, {
     status: 204,
   })
 }
 
 export function BadRequest<T>(payload: T) {
-  return Response.json(error(payload), { status: 400 })
+  return NextResponse.json(error(payload), { status: 400 })
 }
 
 export function Unauthorized<T>(payload?: T) {
-  return Response.json(error(payload), { status: 401 })
+  return NextResponse.json(error(payload), { status: 401 })
 }
 
 export function NotFound<T>(payload: T) {
-  return Response.json(error(payload), { status: 404 })
+  return NextResponse.json(error(payload), { status: 404 })
 }
