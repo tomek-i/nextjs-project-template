565611e8647947d792f78f96db90ad0fdf21eec2
Thomas Iwainski <Tomek.iwainski@gmail.com>
2025-02-26 00:59:53 +1100

    🗑️ remove: obsolete and empty index.ts file

--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-// TODO: create Card component
-// TODO: create Modal component using the new html dialog element
-// TODO: create Overlay element with blur/glass effect
-// TODO: create Dropdown / Select with options, auto-complete
