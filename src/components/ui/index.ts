62d0b6f03b535352f2da249f9d93cf8c459aa2d7
Thomas Iwainski <Tomek.iwainski@gmail.com>
2025-02-26 00:15:29 +1100

    🗑️ remove: old ui components in favour of shadcn

--- src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-// TODO: create label component
-// TODO: create textarea component
-// TODO: create input with label component
-// TODO: create checkbox component
-// TODO: create toggle component
-// TODO: create form component
